0685e14969133e59df722d452d4d6777b3be9254
João Manoel Carvalho <joaomanoel190701@gmail.com>
2025-03-19 22:00:15 -0300

    estrutura inicial

---
+++ src/test/java/br/com/plataforma/lifesync/LifeSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.plataforma.lifesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
